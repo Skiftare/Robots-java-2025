a2eccb252ef678b86c2b72428946b9ff349a524e
skiftare <b.kirill.ig@gmail.com>
2025-02-25 09:08:39 +0500

    Обновлено поведение окон

--- robots/src/main/java/gui/MainApplicationFrame.java
+++ robots/src/main/java/gui/MainApplicationFrame.java
@@ -13,8 +13,33 @@ import javax.swing.JMenuItem;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
+import java.beans.PropertyVetoException;
 
 import log.Logger;
+
+import static java.lang.Math.min;
+import static java.lang.Math.round;
+
+import log.Logger;
+
+import java.awt.*;
+import java.awt.event.KeyEvent;
+
+import javax.swing.JDesktopPane;
+
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
+import java.beans.PropertyVetoException;
+
+import log.Logger;
+
+import static java.lang.Math.min;
+import static java.lang.Math.round;
 
 /**
  * Что требуется сделать:
@@ -23,19 +48,22 @@ import log.Logger;
  */
 public class MainApplicationFrame extends JFrame {
     private final JDesktopPane desktopPane = new JDesktopPane();
+    private int oldWidth = -1;
+    private int oldHeight = -1;
+
 
     public MainApplicationFrame() {
         //Make the big window be indented 50 pixels from each edge
         //of the screen.
         int inset = 50;
-        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+        Rectangle screenBounds = gd.getDefaultConfiguration().getBounds();
+        Dimension screenSize = new Dimension(screenBounds.width, screenBounds.height);
         setBounds(inset, inset,
                 screenSize.width,
                 screenSize.height);
 
         setContentPane(desktopPane);
-
-
 
 
         GameWindow gameWindow = new GameWindow();
@@ -48,7 +76,58 @@ public class MainApplicationFrame extends JFrame {
 
         setJMenuBar(generateMenuBar());
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+        addComponentListener(new ComponentAdapter() {
+            @Override
+            public void componentResized(ComponentEvent e) {
+                resizeInternalFrames();
+            }
+        });
+
     }
+
+    private void resizeInternalFrames() {
+        SwingUtilities.invokeLater(() -> {
+            int width = desktopPane.getWidth();
+            int height = desktopPane.getHeight();
+
+            if (width == 0 || height == 0 || oldWidth <= 0 || oldHeight <= 0) {
+                Logger.debug("Ошибка: размер desktopPane некорректен или ещё не инициализирован!");
+                oldWidth = width;
+                oldHeight = height;
+                return;
+            }
+
+            Logger.debug("Изменяем размеры окон: " + width + "x" + height);
+
+            for (JInternalFrame frame : desktopPane.getAllFrames()) {
+                double widthRatio = (double) frame.getWidth() / oldWidth;
+                double heightRatio = (double) frame.getHeight() / oldHeight;
+                double xRatio = (double) frame.getX() / oldWidth;
+                double yRatio = (double) frame.getY() / oldHeight;
+
+                int newWidth = (int) round(width * widthRatio);
+                int newHeight = (int) round(height * heightRatio);
+                int newX = (int) round(width * xRatio);
+                int newY = (int) round(height * yRatio);
+
+                // Ограничения, чтобы окна не выходили за границы
+                newWidth = Math.min(newWidth, width - newX);
+                newHeight = Math.min(newHeight, height - newY);
+
+                frame.setBounds(newX, newY, newWidth, newHeight);
+                frame.revalidate();
+                frame.repaint();
+            }
+
+            // Сохраняем новые размеры
+            oldWidth = width;
+            oldHeight = height;
+
+            desktopPane.revalidate();
+            desktopPane.repaint();
+        });
+    }
+
 
     protected LogWindow createLogWindow() {
         LogWindow logWindow = new LogWindow(Logger.getDefaultLogSource());
@@ -64,36 +143,6 @@ public class MainApplicationFrame extends JFrame {
         desktopPane.add(frame);
         frame.setVisible(true);
     }
-
-//    protected JMenuBar createMenuBar() {
-//        JMenuBar menuBar = new JMenuBar();
-// 
-//        //Set up the lone menu.
-//        JMenu menu = new JMenu("Document");
-//        menu.setMnemonic(KeyEvent.VK_D);
-//        menuBar.add(menu);
-// 
-//        //Set up the first menu item.
-//        JMenuItem menuItem = new JMenuItem("New");
-//        menuItem.setMnemonic(KeyEvent.VK_N);
-//        menuItem.setAccelerator(KeyStroke.getKeyStroke(
-//                KeyEvent.VK_N, ActionEvent.ALT_MASK));
-//        menuItem.setActionCommand("new");
-////        menuItem.addActionListener(this);
-//        menu.add(menuItem);
-// 
-//        //Set up the second menu item.
-//        menuItem = new JMenuItem("Quit");
-//        menuItem.setMnemonic(KeyEvent.VK_Q);
-//        menuItem.setAccelerator(KeyStroke.getKeyStroke(
-//                KeyEvent.VK_Q, ActionEvent.ALT_MASK));
-//        menuItem.setActionCommand("quit");
-
-    /// /        menuItem.addActionListener(this);
-//        menu.add(menuItem);
-// 
-//        return menuBar;
-//    }
 
 
     private JMenuBar generateMenuBar() {
