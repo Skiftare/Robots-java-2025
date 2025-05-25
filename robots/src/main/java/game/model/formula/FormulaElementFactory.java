8bf542aaab7bb974ea30188bec009690691c0ab8
Berdichevski <ar.berdiich@ya.ru>
2025-05-25 14:52:00 +0500

    part of localisation

--- robots/src/main/java/game/model/formula/FormulaElementFactory.java
+++ robots/src/main/java/game/model/formula/FormulaElementFactory.java
@@ -1,6 +1,8 @@
 package game.model.formula;
 
 import game.model.ObjectProperty;
+import gui.system.localization.LocalizationManager;
+
 import java.awt.Color;
 import java.util.HashMap;
 import java.util.Map;
@@ -9,13 +11,19 @@ public class FormulaElementFactory {
     private static final Map<ObjectProperty, String> propertyToText = new HashMap<>();
     private static final Map<FormulaElement.ElementType, Color> typeColors = new HashMap<>();
 
+    private static LocalizationManager localization = LocalizationManager.getInstance();
+
+    private static String getLocalizedObjectName(String key) {
+        return localization.getString("game.formula." + key);
+    }
+
     static {
         // Map properties to their text representation
-        propertyToText.put(ObjectProperty.PLAYER, "YOU");
-        propertyToText.put(ObjectProperty.PUSHABLE, "PUSH");
-        propertyToText.put(ObjectProperty.STOP, "STOP");
-        propertyToText.put(ObjectProperty.WIN, "WIN");
-        propertyToText.put(ObjectProperty.KILL, "KILL");
+        propertyToText.put(ObjectProperty.PLAYER, getLocalizedObjectName("you"));
+        propertyToText.put(ObjectProperty.PUSHABLE, getLocalizedObjectName("push"));
+        propertyToText.put(ObjectProperty.STOP, getLocalizedObjectName("stop"));
+        propertyToText.put(ObjectProperty.WIN, getLocalizedObjectName("win"));
+        propertyToText.put(ObjectProperty.KILL, getLocalizedObjectName("kill"));
 
         // Define colors for different formula element types
         typeColors.put(FormulaElement.ElementType.NOUN, new Color(230, 180, 80));    // Yellow/gold
@@ -32,7 +40,7 @@ public class FormulaElementFactory {
 
     public static FormulaElement createVerb(int x, int y) {
         FormulaElement element = new FormulaElement(x, y, null,
-                FormulaElement.ElementType.VERB, "IS");
+                FormulaElement.ElementType.VERB, getLocalizedObjectName("is"));
         element.setColor(typeColors.get(FormulaElement.ElementType.VERB));
         return element;
     }
